4b6c53c01a113ac4d5809a5e2d9168874df74fc6
starriest <starriest30@gmail.com>
2019-09-05 15:05:54 +0900

    Create 2563_색종이_지윤.cpp

---
+++ week4/2563_색종이_지윤.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<queue>
+
+using namespace std;
+int map[101][101] = { 0, };
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0); std::cout.tie(0);
+	
+	int num, x, y;
+
+	cin >> num;
+
+	for (int i = 0; i < num; i++) {
+		cin >> x >> y;
+	
+		for (int r = 0; r < 10; r++) {
+			for (int c = 0; c < 10; c++) {
+				map[100 - y  -r][100- x - c] = 1;
+			}
+		}
+	}
+
+	int cnt = 0;
+	for (int r = 0; r < 100; r++) {
+		for (int c = 0; c < 100; c++) {
+			if (map[r][c] == 1) cnt++;
+		}
+	}
+
+	cout << cnt;
+	return 0;
+
+
+}
